73026e5744f8b1157a80a106a9f1162724c98600
Pedro Worcel <pedro.worcel@security-assessment.com>
2014-05-22 14:59:08 +1200

    add bruteforcing

--- plugins/drupal.py
+++ plugins/drupal.py
@@ -1,8 +1,6 @@
 from cement.core import handler, controller
 from plugins import BasePlugin
-import traceback
-import mock
-import sys
+import requests
 
 class DrupalScanner(BasePlugin):
 
@@ -14,7 +12,17 @@ class DrupalScanner(BasePlugin):
         self.enumerate_route()
 
     def enumerate_plugins(self, url):
-        pass
+        plugins = self.modules_get()
+        found_plugins = []
+        for plugin in plugins:
+            r = requests.get("%ssites/all/modules/%s/" % (url, plugin))
+            if r.status_code == 403:
+                found_plugins.append(plugin)
+
+        return found_plugins
+
+    def modules_get(self):
+        return True
 
 def load():
     handler.register(DrupalScanner)
--- tests.py
+++ tests.py
@@ -1,14 +1,20 @@
 from cement.core import controller, foundation, backend
 from cement.utils import test
 from droopescan import DroopeScan
-from mock import patch, MagicMock
 from plugins.drupal import DrupalScanner
-import common
-import droopescan
+from plugins.drupal import DrupalScanner
+from mock import patch, MagicMock
+import requests
+import responses
 
 class BaseTest(test.CementTestCase):
     app_class = DroopeScan
-    base_url = ["--url", "http://example.com/"]
+    scanner = None
+
+    base_url = "http://example.com/"
+
+    param_base_url = ["--url", base_url]
+    param_plugins = param_base_url + ["-e", 'p']
 
     def setUp(self):
         super(BaseTest, self).setUp()
@@ -22,8 +28,15 @@ class BaseTest(test.CementTestCase):
     def tearDown(self):
         self.app.close()
 
-    def mock(self, plugin_label, method):
+    def mock_controller(self, plugin_label, method, return_value = None):
+        """
+            Mocks controller by label. Can only be used to test controllers
+            that get instantiated automatically by cement.
+        """
         m = MagicMock()
+        if return_value:
+            m.return_value = return_value
+
         setattr(backend.__handlers__['controller'][plugin_label], method, m)
         return m
 
@@ -57,20 +70,20 @@ class DroopeScanTest(BaseTest):
     def test_errors_when_no_enumerate(self):
         # add url to make sure that it does not error because of the url
         self.add_argv(["drupal"])
-        self.add_argv(self.base_url)
+        self.add_argv(self.param_base_url)
         self.app.run()
 
     @test.raises(RuntimeError)
     def test_errors_when_invalid_enumerate(self):
         self.add_argv(["drupal"])
-        self.add_argv(self.base_url + ["-e", 'z'])
+        self.add_argv(self.param_base_url + ["-e", 'z'])
         self.app.run()
 
     def test_calls_plugin(self):
         self.add_argv(["drupal"])
-        self.add_argv(self.base_url + ["-e", 'p'])
+        self.add_argv(self.param_plugins)
 
-        m = self.mock('drupalscanner', 'enumerate_plugins')
+        m = self.mock_controller('drupalscanner', 'enumerate_plugins')
 
         self.app.run()
         assert m.called, "enumerate_plugins should have been called given the arguments"
@@ -83,5 +96,25 @@ class DrupalTest(BaseTest):
     def setUp(self):
         super(DrupalTest, self).setUp()
         self.add_argv(["drupal"])
+        self.scanner = DrupalScanner()
+
+    @patch.object(DrupalScanner, 'modules_get', return_value=["nonexistant1",
+        "nonexistant2", "supermodule"])
+    @responses.activate
+    def test_returns_list_of_modules(self, m):
+        responses.add(responses.GET,
+                "%ssites/all/modules/supermodule/" % self.base_url,
+                body='403', status=403)
+        responses.add(responses.GET,
+                "%ssites/all/modules/nonexistant1/" % self.base_url,
+                body='200', status=200)
+        responses.add(responses.GET,
+                "%ssites/all/modules/nonexistant2/" % self.base_url,
+                body='200', status=200)
+
+        result = self.scanner.enumerate_plugins(self.base_url)
+
+        assert result == ["supermodule"], "Should have detected the \
+                'supermodule' module."
 
 
