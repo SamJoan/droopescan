6b23e06ed34348ca1dd85caa15582296dcaea4cd
Pedro Worcel <pedro.worcel@security-assessment.com>
2014-09-06 15:53:02 -0400

    add dnn interesting URLs

--- plugins/dnn.py
+++ plugins/dnn.py
@@ -4,11 +4,15 @@ import common
 
 class Dnn(BasePlugin):
 
+    interesting_urls = [
+            ("Install/UpgradeWizard.aspx", "Upgrade wizard, version disclosure."),
+            ("Install/InstallWizard.aspx", "Install wizard.")
+        ]
+
     versions_file = "plugins/dnn/versions.xml"
 
     can_enumerate_themes = False
     can_enumerate_plugins = False
-    can_enumerate_interesting = False
 
     class Meta:
         label = 'dnn'
