1f6d29077b5b8fb24ef0d2a9367c0c2a48926302
Pedro Worcel <pedro.worcel@security-assessment.com>
2014-09-06 16:09:39 -0400

    add interesting URLs for silverstripe

--- plugins/silverstripe.py
+++ plugins/silverstripe.py
@@ -4,19 +4,21 @@ import common
 
 class SilverStripe(BasePlugin):
 
-    plugins_file = "plugins/silverstripe/wordlists/plugins"
+    plugins_file = 'plugins/silverstripe/wordlists/plugins'
     plugins_base_url = '%s%s/'
 
-    themes_file = "plugins/silverstripe/wordlists/themes"
+    themes_file = 'plugins/silverstripe/wordlists/themes'
     themes_base_url = '%sthemes/%s/'
 
-    folder_url = "framework/"
-    regular_file_url = ["cms/css/layout.css", "framework/css/UploadField.css"]
-    module_readme_file = "README.md"
+    folder_url = 'framework/'
+    regular_file_url = ['cms/css/layout.css', 'framework/css/UploadField.css']
+    module_readme_file = 'README.md'
 
-    versions_file = "plugins/silverstripe/versions.xml"
+    versions_file = 'plugins/silverstripe/versions.xml'
 
-    can_enumerate_interesting = False
+    interesting_urls = [
+            ('framework/docs/en/changelogs/index.md', 'Changelogs, there are other files in same dir, but \'index.md\' is frequently outdated.'),
+        ]
 
     class Meta:
         label = 'silverstripe'
