34a7c9ba87c8ac6c09ef6ec89345570be22fb1b7
Pedro Worcel <pedro@worcel.com>
2015-08-16 03:45:01 -0400

    Add info disclosure URL for SS.

--- dscan/plugins/silverstripe.py
+++ dscan/plugins/silverstripe.py
@@ -33,7 +33,8 @@ class SilverStripe(BasePlugin):
     interesting_urls = [
             ('framework/docs/en/changelogs/index.md', 'Changelogs, there are other files in same dir, but \'index.md\' is frequently outdated.'),
             ('Security/login', 'Administrative interface.'),
-            ('composer.json', 'Contains detailed, sensitive dependency information.')
+            ('composer.json', 'Contains detailed, sensitive dependency information.'),
+            ('vendor/composer/installed.json', 'Contains detailed, sensitive dependency information.'),
         ]
 
     interesting_module_urls = [
